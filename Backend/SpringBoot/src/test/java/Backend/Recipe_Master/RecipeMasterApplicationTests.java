ca5f8ac0090fee273f50cb0685f3c1120ee110db
RajithmaHaseli <rajithmahaseli2001@gmail.com>
2024-11-20 12:35:08 +0530

    Add node_modules to repository

---
+++ Backend/SpringBoot/src/test/java/Backend/Recipe_Master/RecipeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Recipe_Master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
